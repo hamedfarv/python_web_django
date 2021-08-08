fa73ee625c1922c625593f26b713e7786c207219
hamedfarv <hamedfarvardin@gmail.com>
2021-08-08 11:33:50 +0430

    display products values in index.html

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Products ,Offer
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','price','stock')
+  
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code','discount')
+  
+
+admin.site.register(Products,ProductAdmin) ## pass Products class as argument
+admin.site.register(Offer,OfferAdmin)
--- products/views.py
+++ products/views.py
@@ -1,14 +1,14 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import Products
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello Hamed')
+   products =  Products.objects.all()
+   return render(request, 'index.html', {'my_products': products})
+
 
 def newprod(request):
     return HttpResponse('new products')
-
-
-
